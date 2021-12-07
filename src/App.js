9f7a6ac80b3ed4c1fdc007813d09089295093067
Stefana <farcasus@yahoo.com>
2021-12-07 19:59:22 +0200

    search-box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component.jsx";
-
+import { SearchBox } from "./components/search-box/search-box-component";
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
   componentDidMount() {
@@ -14,10 +15,23 @@ class App extends Component {
       .then((resp) => resp.json())
       .then((users) => this.setState({ monsters: users }));
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
+    const { monsters, searchField } = this.state;
+    const filtratedMonsters = monsters.filter((monster) => {
+      return monster.name
+        .toLowerCase()
+        .includes(searchField.toLocaleLowerCase());
+    });
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filtratedMonsters}></CardList>
       </div>
     );
   }
---
+++ src/components/search-box/search-box-component.jsx
@@ -0,0 +1,11 @@
+import "./search-box.styles.css";
+export const SearchBox = ({ placeholder, handleChange }) => {
+  return (
+    <input
+      type="search"
+      className="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+    />
+  );
+};
